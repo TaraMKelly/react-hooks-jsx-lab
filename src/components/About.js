f5c88a08187b4bc125a1f1fb54c253d371bcfa7d
TaraMKelly <tara.kelly16@gmail.com>
2021-07-19 12:00:58 -0400

    commit tree 8a5d83c58ceaf9bb96ae6144ab3866d50bd4c181

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,20 @@
 import React from "react";
 import { image } from "../data/data";
 
+// a <div> element with an id of "about"
+// inside the div element, there should be three child elements:
+// a <h2> element with the content of "About Me"
+// a <p> element with any content of your choosing
+// a <img> element with the src attribute set to the image variable, and the alt attribute set to "I made this"
+
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Hi, I'm Tara.</p>
+      <img src={ image } alt="I made this" />
+    </div>
+  )
 }
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,26 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+// Inside the <Home> component, there are a couple of variables defined that will allow you to insert your name and city into the portfolio page.
+
+// The <Home> component should return the following JSX elements:
+
+// a <div> element with an id of "home"
+// inside the div element, there should be one child element:
+// a <h1> element, with the style attribute set to a color of "firebrick", and the content of "Your Name is a Web Developer from Your City"
+// use the imported name and city variables within the component, and the JSX expression syntax to use the variables inside your JSX elements
+
+const h1Style = {
+  color: "firebrick"
+}
+
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+
+  return (
+    <div id="home">
+      <h1 style={h1Style}>{name} is a Web Developer from {city}</h1>
+    </div>
+  )
 }
 
 export default Home;
